1f8900ec0955e6c56a07167f4421daff57c577ed
Sitouxz <yehezkielowen0901@gmail.com>
2021-10-09 03:18:25 +0800

    Add modal, toast, code desc & some bug Fix

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Made by Yehezkiel Owen Ombuh
+// with help from Docs, StackOverflow, W3school and other website
+// ===========
+
+// Add Student Button Text Change
 function showHide() {
     const btn = document.getElementById('showBtn');
     if (btn.innerHTML == 'Hide Form Add New Student') {
@@ -6,7 +11,9 @@ function showHide() {
         btn.innerHTML = 'Hide Form Add New Student';
     }
 }
+// End of (Add Student Button Text Change)
 
+// Bootstrap Validation Script
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
@@ -26,11 +33,12 @@ function showHide() {
                 form.classList.add('was-validated')
             }, false)
         })
-});
+})()
+// End of (Bootstrap Validation Script)
 
+// Cascading Drop Down
 // Source: https://www.w3schools.com/howto/howto_js_cascading_dropdown.asp
-
-let cascading = {
+let facultyList = {
     'Akademi Sekretari Manajemen Indonesia Klabat': ['Sekretari (D3)'],
     'Fakultas Ekonomi dan Bisnis': ['Akuntansi', 'Manajemen'],
     'Fakultas Filsafat': ['Ilmu Filsafat'],
@@ -43,24 +51,20 @@ let cascading = {
 window.onload = function () {
     let faculty = document.getElementById('formFaculty');
     let program = document.getElementById('formProgramOfStudy');
-    for (let x in cascading) {
+    for (let x in facultyList) {
         faculty.options[faculty.options.length] = new Option(x, x);
     }
     faculty.onchange = function () {
         program.length = 1;
-        let z = cascading[this.value];
+        let z = facultyList[this.value];
         for (let i = 0; i < z.length; i++) {
             program.options[program.options.length] = new Option(z[i], z[i])
         }
     }
-
-    // let showFaculty = document.getElementById('selectionFaculty');
-    // let showProgram = document.getElementById('selectionProgramOfStudy');
-    // for (let a in cascading) {
-    //     showFaculty.options[showFaculty.options.length] = new Option(a);
-    // }
 }
+// End of (Cascading Drop Down)
 
+// Student Database
 let students = [{
         id: 0,
         nim: 105011810001,
@@ -86,30 +90,19 @@ let students = [{
         programOfStudy: 'Informatika'
     }
 ]
+// End of (Student Database)
 
+// Get data from input and submit button
 const inputNim = document.querySelector('input[name="nim"]');
 const inputFullName = document.querySelector('input[name="fullName"]');
 const inputFemale = document.querySelector('input#male');
 const inputMale = document.querySelector('input#female');
-// let inputGender = '';
-
-// if (inputFemale.checked == true) {
-//     inputGender = 'Female';
-// } else {
-//     inputGender = 'Male';
-// }
 const inputFormFaculty = document.querySelector('select#formFaculty');
 const inputFormProgramOfStudy = document.querySelector('select#formProgramOfStudy');
-// const newStudent = {
-//     nim: inputNim,
-//     fullname: inputFullName,
-//     gender: inputGender,
-//     faculty: inputFormFaculty,
-//     programOfStudy: inputFormProgramOfStudy
-// }
-
 const formSubmit = document.querySelector('#formSubmit');
+// End of (Get data from input and submit button)
 
+// Send data to Student Database
 const addStudent = () => {
     const nim = inputNim.value;
     const fullName = inputFullName.value;
@@ -124,6 +117,10 @@ const addStudent = () => {
     }
     const gender = inputGender;
 
+    if (nim == '' || fullName == '' || formFaculty == '' || formProgramOfStudy == '') {
+
+        return;
+    }
 
     const newStudent = {
         nim: nim,
@@ -133,22 +130,39 @@ const addStudent = () => {
         programOfStudy: formProgramOfStudy
     }
 
-
-    console.log(newStudent);
     students.push(newStudent)
     renderData();
+    var addToast = document.getElementById('addToast')
+    var toast = new bootstrap.Toast(addToast)
+
+    toast.show()
 }
+// End of (Send data to Student Database)
 
-
+// Delete Student
+let dataRow;
+let deleteName;
 
 function deleteStudent(data) {
     var row = data.closest('tr').rowIndex - 1;
-    if (confirm("Are you sure?")) {
-        students.splice(row, 1);
-    }
-    renderData();
+    var parentRow = data.parentNode.parentNode;
+    var name = parentRow.querySelector('td.fullName').textContent;
+    deleteName = name;
+    console.log(name);
+    document.getElementById('deleteModalText').innerHTML = `Are you sure to delete <span class="bg-secondary py-0 px-1 rounded text-light">${deleteName}</span>?`
 }
 
+function confirmDelete() {
+    students.splice(dataRow, 1);
+    renderData();
+    var delToast = document.getElementById('deleteToast');
+    var toast = new bootstrap.Toast(delToast);
+    toast.show()
+}
+// End of (Send data to Student Database)
+
+
+// Render data
 const table = document.querySelector('#studentsTable');
 
 const renderData = () => {
@@ -170,10 +184,12 @@ const renderData = () => {
         programOfStudy.className = 'programOfStudy';
         programOfStudy.innerHTML = student.programOfStudy;
         let action = row.insertCell(5);
-        action.innerHTML = '<button class="btn btn-danger" onclick="deleteStudent(this);"><i class="bi bi-trash-fill"></i></button>';
+        action.innerHTML = '<button class="btn btn-danger" onclick="deleteStudent(this);" data-bs-toggle="modal" data-bs-target="#deleteAlert"><i class="bi bi-trash-fill"></i></button>';
     }
 }
+//End of (Render data)
 
+// Filter by Faculty
 function filterFaculty() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("selectionFaculty");
@@ -192,8 +208,14 @@ function filterFaculty() {
             }
         }
     }
+    if (input.value == '-- SELECT FACULTY --') {
+        renderData();
+    }
+    input.value = '-- SELECT FACULTY --';
 }
+// End of (Filter by Faculty)
 
+// Filter by Program of Study
 function filterProgramOfStudy() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("selectionProgramOfStudy");
@@ -212,8 +234,14 @@ function filterProgramOfStudy() {
             }
         }
     }
+    if (input.value == '-- SELECT PROGRAM OF STUDY --') {
+        renderData();
+    }
+    input.value = '-- SELECT PROGRAM OF STUDY --';
 }
+// End of (Filter by Program of Study)
 
+// Search by name
 function searchName() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("searchInput");
@@ -233,13 +261,11 @@ function searchName() {
         }
     }
 }
-
-// var e = document.getElementById('selectionFaculty');
-// var strE = e.options[e.selectedIndex].text;
-// console.log(strE);
+// End of (Search by name)
 
 // Pre rendered function & data
 renderData();
+
 formSubmit.addEventListener('click', addStudent);
 
 var form = document.getElementById("studentForm");
@@ -248,3 +274,4 @@ function handleForm(event) {
     event.preventDefault();
 }
 form.addEventListener('submit', handleForm);
+// End of (Pre rendered function & data)
